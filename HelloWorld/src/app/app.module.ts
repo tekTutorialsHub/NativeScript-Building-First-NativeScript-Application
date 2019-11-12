29fda62f3168150b27a764ca1d85a338391e4aab
chandranath <chandranath@bhoomiinfosolutions.com>
2019-11-12 22:35:21 +0530

    Updated to Nativescript 6.2.0

---
+++ HelloWorld/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+ 
+import { Routes } from "@angular/router";
+ 
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home.component";
+ 
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+];
+ 
+ 
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        NativeScriptRouterModule,NativeScriptRouterModule.forRoot(routes)
+    ],
+    declarations: [
+        AppComponent, HomeComponent
+    ],
+    providers: [
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+ 
+export class AppModule { }
