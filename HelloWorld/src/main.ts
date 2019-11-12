e10279988720777f2dfbc8332a671df2a6831b28
TekTutorialshub <admin@tektutorialshub.com>
2019-11-12 22:35:21 +0530

    Updated to Nativescript 6.2.0

---
+++ HelloWorld/src/main.ts
@@ -0,0 +1,3 @@
+import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+import { AppModule } from "./app/app.module";
+platformNativeScriptDynamic().bootstrapModule(AppModule);
